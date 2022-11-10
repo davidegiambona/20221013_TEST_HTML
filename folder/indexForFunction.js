582c98eeb791254e281bdf05542ad6a82396445f
Davide Giambona <86895372+davidegiambona@users.noreply.github.com>
2022-11-10 20:44:53 +0100

    Update indexForFunction.js

--- folder/indexForFunction.js
+++ folder/indexForFunction.js
@@ -1,1 +1,9 @@
+var number = 0;
 
+function takeNumberAndEdit() {
+    document.write("numero originario: ", number, ", ");
+    var numberChanged = 13;
+    number = numberChanged;
+    document.write("\n numero cambiato: ", number);
+}
+takeNumberAndEdit();
