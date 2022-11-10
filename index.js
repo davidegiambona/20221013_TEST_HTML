f9d3cbcdd7bb063bb8841e846e47322c6599513a
Davide Giambona <86895372+davidegiambona@users.noreply.github.com>
2022-11-10 20:48:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-console.log("TEST 2022-11-3");
-let x = 34;
-console.log(typeof(x))
+var number = 0;
 
-let myString = "my first string";
-console.log("type of string ->", typeof(mystring))
-console.log("myStrin ->", myString)
-console.log("myString ->", myString)
-
-let myStringDoubleApo = "my \"first string\"";
-console.log("myStringDoubleApo ->", myStringDoubleApo)
-
-let myStringBSlash= "my \\ \"first string\"";
-console.log("myStringBSlash ->", myStringBSlash)
-
-let myStringNLine = "my \n \"first string\"";
-console.log("myStringNLine ->", myStringNLine)
-console.log("myStringNLine ->", myStringNLine.lenght)
-
-let myStringOrig = "my first string";
-let myStringOrigSliced = "my fist string".slice(3, 6);
-console.log("myStringOrig -- myStringOrigSliced ->", myStringOrig, "--", myStringOrigSliced)
-
-let myStringOrigSlicedTwo = "myStringOrigSlicedTwo".slice(3);
-console.log("myStringOrigSlicedTwo ->", myStringOrigSlicedTwo)
-
-let myStringOrigSlicedThree = "my first string".slice(-3)
-console.log("myStringOrigSlicedThree ->", myStringOrigSlicedThree)
-
-let strCharAt = "Test"
-console.log(strCharAt, strCharAt.charAt(2, 1))
+function takeNumberAndEdit() {
+    document.write("numero originario: ", number, ", ");
+    var numberChanged = 13;
+    number = numberChanged;
+    document.write("\n numero cambiato: ", number);
+}
+takeNumberAndEdit();
